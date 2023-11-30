870fe005fc4ae94a415ca353c95d32c1e9536801
Jack Miller <jdmiller1991@outlook.com>
2023-11-30 10:02:59 +0000

    Latest update

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -1,18 +1,44 @@
-import './AboutMe.css'
-import JackPic from './assets/JackPic.jpeg'
+import { useEffect, useState } from 'react';
+import './AboutMe.css';
+import JackPic from './assets/JackPic.jpeg';
 
 export default function AboutMe() {
+    // Initial tea count
+    const initialTeaCount = 4578;
+
+    // State to hold the current count of teas
+    const [teaCount, setTeaCount] = useState(initialTeaCount);
+
+    useEffect(() => {
+        // Set up an interval to increase the tea count every 10 seconds
+        const interval = setInterval(() => {
+            setTeaCount((prevCount) => prevCount + 1);
+        }, 10000); // 10000 milliseconds = 10 seconds
+
+        // Clear the interval when the component unmounts
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <div className="page-fade-in">
-        <div className="aboutme-wrapper">
-            <div className='aboutme-image-container'>
-                <img src={JackPic} alt="Jack" className="aboutme-image" />
+            <div className="aboutme-wrapper">
+                <div className='aboutme-image-container'>
+                    <img src={JackPic} alt="Jack" className="aboutme-image" />
+                </div>
+                <div className='aboutme-details'>
+                    <h2>Jack Miller</h2>
+                    <br />
+                    <h3>Experience</h3><span>6 Months</span>
+                    <br />
+                    <h3>Projects completed:</h3><span>6+</span>
+                    <br />
+                    <h3>Cups of tea Consumed:</h3><span id="teaCount">{teaCount.toLocaleString()}</span>
+                </div>
+                <div className="aboutme-content">
+                    <h2>About Me:</h2>
+                    <p>Your about me description...</p>
+                </div>
             </div>
-            <div className="aboutme-content">
-                <h2>About Me:</h2>
-                <p>I am a dynamic and innovative Junior Full-Stack Developer with a passion for creating efficient and user-friendly web applications. Possessing a strong foundation in both front-end and back-end technologies, I am adept at utilizing languages such as JavaScript, HTML, CSS, and frameworks like React and Node.js. My background includes working on collaborative projects where I contributed to the development, testing, and deployment of web applications. With a keen eye for design and functionality, I continuously strive to enhance user experience and optimize application performance. Eager to bring my technical skills and fresh perspectives to a forward-thinking development team, where I can contribute to challenging projects and grow professionally.</p>
-            </div>
-        </div>
         </div>
     );
 }
