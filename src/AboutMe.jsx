9039958decf16e75a070d3daab8410920e40d1dd
Jack Miller <jdmiller1991@outlook.com>
2023-12-01 12:59:19 +0000

    Latest update

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -1,42 +1,60 @@
-import { useEffect, useState } from 'react';
-import './AboutMe.css';
-import JackPic from './assets/JackPic.jpeg';
+import { useEffect, useState } from "react";
+import "./AboutMe.css";
+import JackPic from "./assets/JackPic.jpeg";
 
 export default function AboutMe() {
-    // Initial tea count
-    const initialTeaCount = 4578;
+  // Initial tea count
+  const initialTeaCount = 4578;
 
-    // State to hold the current count of teas
-    const [teaCount, setTeaCount] = useState(initialTeaCount);
+  // State to hold the current count of teas
+  const [teaCount, setTeaCount] = useState(initialTeaCount);
 
-    useEffect(() => {
-        // Set up an interval to increase the tea count every 10 seconds
-        const interval = setInterval(() => {
-            setTeaCount((prevCount) => prevCount + 1);
-        }, 10000); // 10000 milliseconds = 10 seconds
+  useEffect(() => {
+    // Set up an interval to increase the tea count every 10 seconds
+    const interval = setInterval(() => {
+      setTeaCount((prevCount) => prevCount + 1);
+    }, 10000); // 10000 milliseconds = 10 seconds
 
-        // Clear the interval when the component unmounts
-        return () => clearInterval(interval);
-    }, []);
+    // Clear the interval when the component unmounts
+    return () => clearInterval(interval);
+  }, []);
 
-    return (
-            <div className="aboutme-wrapper">
-                <div className='aboutme-image-container'>
-                    <img src={JackPic} alt="Jack" className="aboutme-image" />
-                </div>
-                <div className='aboutme-details'>
-                    <h2>Jack Miller</h2>
-                    <br />
-                    <h3>Experience</h3><span>6 Months</span>
-                    <br />
-                    <h3>Projects completed:</h3><span>6+</span>
-                    <br />
-                    <h3>Cups of tea Consumed:</h3><span id="teaCount">{teaCount.toLocaleString()}</span>
-                </div>
-                <div className="aboutme-content">
-                    <h2>About Me:</h2>
-                    <p>As a motivated and skilled Junior Full Stack Software Developer, I bring a unique blend of foundational knowledge in both front-end and back-end technologies. My experience includes developing responsive user interfaces, implementing dynamic client-side scripting, and building robust server-side applications. <br /><br /> With a strong commitment to writing clean, maintainable code, I excel in collaborative environments where my passion for learning and my ability to adapt to new technologies and frameworks contribute to the success of complex software projects. My proactive approach to problem-solving and eagerness to embrace the latest industry trends make me a valuable asset to any development team seeking innovation and efficiency.</p>
-                </div>
-            </div>
-    );
+  return (
+    <body className="aboutme-body">
+      <div className="aboutme-wrapper">
+        <div className="aboutme-image-container">
+          <img src={JackPic} alt="Jack" className="aboutme-image" />
+        </div>
+        <div className="aboutme-details">
+          <h2>Jack Miller</h2>
+          <br />
+          <h3>Experience</h3>
+          <span>6 Months</span>
+          <br />
+          <h3>Projects completed:</h3>
+          <span>6+</span>
+          <br />
+          <h3>Cups of tea Consumed:</h3>
+          <span id="teaCount">{teaCount.toLocaleString()}</span>
+        </div>
+        <div className="aboutme-content">
+          <h2>About Me:</h2>
+          <p>
+            As a motivated and skilled Junior Full Stack Software Developer, I
+            bring a unique blend of foundational knowledge in both front-end and
+            back-end technologies. My experience includes developing responsive
+            user interfaces, implementing dynamic client-side scripting, and
+            building robust server-side applications. <br />
+            <br /> With a strong commitment to writing clean, maintainable code,
+            I excel in collaborative environments where my passion for learning
+            and my ability to adapt to new technologies and frameworks
+            contribute to the success of complex software projects. My proactive
+            approach to problem-solving and eagerness to embrace the latest
+            industry trends make me a valuable asset to any development team
+            seeking innovation and efficiency.
+          </p>
+        </div>
+      </div>
+    </body>
+  );
 }
--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,17 +1,46 @@
 import "./Portfolio.css";
+import DashboardChallenge from "./assets/DashboardChallenge.jpg";
+import GreengrocersChallenge from "./assets/GreengrocersChallenge.jpg";
+import PersonalProject from "./assets/PersonalProject.jpg";
 
 export default function Portfolio() {
   return (
-    <div className="portfolio-container">
-      <div className="project-1">
-        <p>hvdbhkdbv</p>
+    <body className="portfolio-page">
+      <div className="portfolio-container">
+        <h1 className="portfolio-header">
+          Hover over a tile for more information
+        </h1>
+        <div className="project-1">
+          <div className="project-front">
+            <h3>The Cohort Manager Project</h3>
+            <img src={DashboardChallenge} alt="Dashboard" />
+          </div>
+          <div className="project-back">
+            <p>Description of Cohort Manager Project...</p>
+            <a href="link-to-cohort-manager">View Project</a>
+          </div>
+        </div>
+        <div className="project-2">
+          <div className="project-front">
+            <h3>Workout & Ice Bath Tracker</h3>
+            <img src={PersonalProject} alt="Personal" />
+          </div>
+          <div className="project-back">
+            <p>Description of Workout & Ice Bath Tracker...</p>
+            <a href="link-to-workout-tracker">View Project</a>
+          </div>
+        </div>
+        <div className="project-3">
+          <div className="project-front">
+            <h3>The Greengrocers Project</h3>
+            <img src={GreengrocersChallenge} alt="Greengrocers" />
+          </div>
+          <div className="project-back">
+            <p>Description of Greengrocers Project...</p>
+            <a href="link-to-greengrocers">View Project</a>
+          </div>
+        </div>
       </div>
-      <div className="project-2">
-        <p>bidsbvkjbsdv</p>
-      </div>
-      <div className="project-3">
-        <p>byigbibv</p>
-      </div>
-    </div>
+    </body>
   );
 }
